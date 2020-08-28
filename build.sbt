76014151f0cab1d6dd4242da2c98cfed178c237f
zevik <p.yazdanian74@gmail.com>
2020-08-28 12:31:46 +0430

    some paralell processing added in collections

--- build.sbt
+++ build.sbt
@@ -10,7 +10,21 @@ libraryDependencies += "org.apache.httpcomponents" % "httpclient" % "4.1.2"
 
 libraryDependencies += "org.mongodb" % "mongo-java-driver" % "2.10.1"
 
-libraryDependencies += "org.scala-lang.modules" %% "scala-xml" % "1.0.2"
+
+resolvers ++= Seq(
+  "Scala-Tools Maven2 Releases Repository" at "http://scala-tools.org/repo-releases",
+  "Scala-Tools Maven2 Snapshots Repository" at "http://scala-tools.org/repo-snapshots"
+)
+
+
+
+
+libraryDependencies ++= Seq(
+  "com.h2database" % "h2" % "1.2.137",
+  "org.squeryl" % "squeryl_2.10" % "0.9.5-6"
+)
+
+
 
 libraryDependencies +=
   "com.typesafe.akka" %% "akka-actor" % "2.3.16"
--- src/main/scala/Collections/ParallelCollections.scala
+++ src/main/scala/Collections/ParallelCollections.scala
@@ -5,18 +5,17 @@
 
 import scala.collection.parallel.immutable._
 
-object ParallelCollectionsExample {
+object ParallelCollectionsExample extends App{
 
   def parallelFilter = {
-    Vector.range(1, 100000).par.filter(_ % 2 == 0).seq
+    Vector.range(1, 1000000).par.filter(_ % 2 == 0).seq
   }
 
   def conversion = {
+
+
     timedOperation {
-      Vector.range(1, 50000).par
-    }
-    timedOperation {
-      val xs = List.iterate(1, 50000)(x => x + 1)
+      val xs = List.iterate(1, 500000)(x => x + 1)
       xs.par
     }
   }
@@ -24,6 +23,34 @@ object ParallelCollectionsExample {
   def timedOperation[T](f: => T) = {
     val startTime = System.currentTimeMillis
     f
-    println("Time taken = " + (System.currentTimeMillis - startTime))
+    println("Time taken = " + (System.currentTimeMillis - startTime) + " ms!")
   }
+
+  def doWork[T](mode:String,input:Vector[Int]) = {
+    val startTime = System.currentTimeMillis
+    mode match {
+      case "PARALLEL" => {println("Parallel"); input.par.filter(_ % 3 == 0).seq}
+      case "SEQ" => {println("Seq"); input.filter(_ % 3 == 0).seq}
+      case _ => println("RIDID")
+
+    }
+    println("Time taken = " + (System.currentTimeMillis - startTime) + " ms!")
+  }
+
+  timedOperation(parallelFilter)
+
+  println("------------")
+
+
+  // ----------- parallel
+  println("Parallel Filtering : ")
+  doWork("PARALLEL",Vector.range(1, 10000000))
+
+  // ----------- regular
+  println("Sequential Filtering : ")
+  doWork("SEQ",Vector.range(1, 10000000))
+
+
+  println("------------")
+  conversion
 }
