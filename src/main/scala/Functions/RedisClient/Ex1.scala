08ffba8fcc7c4886e3d5e278ecfd090969e6fe68
zevik <p.yazdanian74@gmail.com>
2020-09-17 15:40:01 +0430

    more work on redis client example, move it to Functional Folder

--- src/main/scala/Functions/RedisClient/Ex1.scala
+++ src/main/scala/Functions/RedisClient/Ex1.scala
@@ -1,3 +1,5 @@
+package Functions.RedisClient
+
 /**
   * Created by zevik on 8/28/20.
   */
@@ -6,10 +8,10 @@
 import akka.util.ByteString
 import redis.RedisClient
 
-import scala.concurrent.{Await, Future}
-import scala.concurrent.duration._
+import scala.concurrent.Await
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.util.Try
+import scala.concurrent.duration._
+import scala.util.{Failure, Success, Try}
 
 object RedisEx1 extends App {
   implicit val akkaSystem = akka.actor.ActorSystem()
@@ -19,7 +21,7 @@ object RedisEx1 extends App {
   val futurePong = redis.ping()
   println("Ping sent!")
   futurePong.map(pong => {
-    println(s" ----> Redis replied with a $pong\n\n ------------------------")
+    println(s" ----> Redis replied with a $pong\n\n ------------------------\n\n")
   })
   Await.result(futurePong, 5 seconds)
 
@@ -52,22 +54,50 @@ object RedisEx1 extends App {
 
 
   def extractValue(value: Option[Try[Option[ByteString]]]): String = {
-    if (value.get.isSuccess){
-      if (value.get.get.nonEmpty)
-        return value.get.get.get.utf8String
-      return null
+    value.get match {
+      case Success(i) => {
+        i match {
+          case Some(u) => return u.utf8String
+          case None => println("That didn't work. Value Does Not Exists ...");return null;
+        }
+      }
+      case Failure(t) => {
+        throw t.getCause
+      }
     }
-    else
-      return null
+
+
+
+  }
+
+  def extractValueTraditional(value: Option[Try[Option[ByteString]]]): String = {
+
+     // ----------- In Traditional Format ------
+        if (value.get.isSuccess){
+          if (value.get.get.nonEmpty)
+            return value.get.get.get.utf8String
+          return null
+        }
+        else
+          return null
+
   }
 
 
   setValueToRedis("1998","France")
 
 
+  // --------- set value
   val value = getValueFromRedis("1998")
+
+  // -------- get value
   val extractedValue = extractValue(value)
   println(String.format(" Cached value for key '%s' is : ","1998") +extractedValue)
+
+  val extractedValue2 = extractValueTraditional(value)
+  println(String.format(" Cached value for key '%s' is : ","1998") +extractedValue2)
+
+  println("\n\n\n\n")
 
   akkaSystem.shutdown()
 }
